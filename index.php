710af9e0f9952c97e3095f38276983ed2f703ac8
nlquan-png <nlquan@vku.udn.vn>
2025-11-07 15:49:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ngo le quan</title>
+</head>
+<body>
+    <h1>Welcome to Autos Database</h1>
+    <a href="login.php">Please Log In</a>
+    <p>Attempt to go to auto.php without logging in - it should fail with an error message.</p>
+    <a href="https://github.com/nlquan-png/courseraModule2.9"
+ target="_blank">Source Code for this Application</a>
+    <p>Note: Your implementation should retain data across multiple logout/login sessions. This sample implementation clears all its data on logout - which you should not do in your implementation.</p>
+</body>
+</html>
