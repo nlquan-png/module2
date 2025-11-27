7d1f48327bae3ac4ac0ee88a7e88859bedd6cb6a
nlquan-png <nlquan@vku.udn.vn>
2025-11-27 11:48:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ngo le quan</title>
+    <title>Ngô Lê Quân nlquan@vku.udn.vn | 38d08</title>
 </head>
 <body>
     <h1>Welcome to Autos Database</h1>
@@ -14,4 +14,5 @@
  target="_blank">Source Code for this Application</a>
     <p>Note: Your implementation should retain data across multiple logout/login sessions. This sample implementation clears all its data on logout - which you should not do in your implementation.</p>
 </body>
+
 </html>
